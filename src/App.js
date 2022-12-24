7c19a19b2952eea213ceff5df3adf2b40db8ab07
Gor <58237309+Cactus322@users.noreply.github.com>
2022-12-24 21:23:36 +0300

    created custom hook

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
 	useParams,
 	useNavigate,
 } from 'react-router-dom'
+import { useField } from './hooks'
 
 const Menu = () => {
 	const padding = {
@@ -88,22 +89,31 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-	const [content, setContent] = useState('')
-	const [author, setAuthor] = useState('')
-	const [info, setInfo] = useState('')
+	// const [content, setContent] = useState('')
+	// const [author, setAuthor] = useState('')
+	// const [info, setInfo] = useState('')
 	const navigate = useNavigate()
+	const content = useField('')
+	const author = useField('')
+	const info = useField('')
 
 	const handleSubmit = (e) => {
 		e.preventDefault()
 		props.addNew({
-			content,
-			author,
-			info,
+			content: content.value,
+			author: author.value,
+			info: info.value,
 			votes: 0,
 		})
 		navigate('/anecdotes')
 		props.setNotification('created new anecdote')
 		setTimeout(() => props.setNotification(''), 5000)
+	}
+
+	const handleReset = () => {
+		content.onReset()
+		author.onReset()
+		info.onReset()
 	}
 
 	return (
@@ -112,29 +122,22 @@ const CreateNew = (props) => {
 			<form onSubmit={handleSubmit}>
 				<div>
 					content
-					<input
-						name="content"
-						value={content}
-						onChange={(e) => setContent(e.target.value)}
-					/>
+					<input {...content} />
 				</div>
 				<div>
 					author
 					<input
-						name="author"
-						value={author}
-						onChange={(e) => setAuthor(e.target.value)}
+						{...author}
 					/>
 				</div>
 				<div>
 					url for more info
 					<input
-						name="info"
-						value={info}
-						onChange={(e) => setInfo(e.target.value)}
+						{...info}
 					/>
 				</div>
 				<button>create</button>
+				<input type="reset" onClick={handleReset} value="reset" />
 			</form>
 		</div>
 	)
